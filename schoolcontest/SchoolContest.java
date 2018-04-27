f325416f33b0be8dc2f9d7334dc965b672012dfa
Jürgen Geyer <geyer.juergen00@gmail.com>
2018-04-27 09:59:40 +0200

    Tolle zwei Beispiele ich bin super

--- schoolcontest/SchoolContest.java
+++ schoolcontest/SchoolContest.java
@@ -5,6 +5,8 @@
  */
 package schoolcontest;
 
+import java.util.Scanner;
+
 /**
  *
  * @author Jürgen Geyer
@@ -15,7 +17,7 @@ public class SchoolContest {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
     }
     
 }
